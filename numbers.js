7380b5bae654d82adf5fc2a0c48ed15a17e5e733
Adrian Marquis <adrian.marquis78@gmail.com>
2021-01-22 16:03:00 -0500

    working

--- numbers.js
+++ numbers.js
@@ -9,10 +9,20 @@ console.log(total);
 total += 3;
 console.log(total);
 
-total ++;
+total++;
 console.log(total);
 
 function computeArea(width, height) {
     return width * height;
 }
 console.log(computeArea(23, 34));
+
+function celsToFahr(celsTemp) {
+    return (celsTemp * 9 / 5) + 32;
+}
+console.log(celsToFahr(34));
+
+function fahrToCels(fahrTemp) {
+    return (fahrTemp - 32) * 5 / 9;
+}
+console.log(fahrToCels(78));
