997db2c43843412dc0a7140a15fae321da6fd552
Adrian Marquis <adrian.marquis78@gmail.com>
2021-01-22 15:48:58 -0500

    working

--- numbers.js
+++ numbers.js
@@ -1,1 +1,18 @@
 console.log(typeof 7);
+
+let total = 1 + 2 + 3 + 4;
+console.log(total);
+
+total = total + 1;
+console.log(total);
+
+total += 3;
+console.log(total);
+
+total ++;
+console.log(total);
+
+function computeArea(width, height) {
+    return width * height;
+}
+console.log(computeArea(23, 34));
